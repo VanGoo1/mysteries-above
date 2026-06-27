d5029446841bb1bb3171e76869e27a50593b8657
Yumeead <yurypalamarchuk2007@gmail.com>
2026-06-27 23:33:46 +0300

    fix(behaviors): SafeLocations prefers above-origin spots (preserve structure spawn behavior)

--- src/main/java/me/vangoo/infrastructure/creatures/SafeLocations.java
+++ src/main/java/me/vangoo/infrastructure/creatures/SafeLocations.java
@@ -8,7 +8,7 @@ public final class SafeLocations {
     private SafeLocations() {}
 
     public static Location passableNear(Location origin) {
-        int[][] offsets = { {0, 0, 0}, {0, 1, 0}, {1, 0, 0}, {-1, 0, 0}, {0, 0, 1}, {0, 0, -1}, {2, 0, 0}, {0, 0, 2} };
+        int[][] offsets = { {0, 1, 0}, {1, 1, 0}, {-1, 1, 0}, {0, 1, 1}, {0, 1, -1}, {2, 1, 0}, {0, 1, 2}, {0, 0, 0} };
         for (int[] o : offsets) {
             Location cand = origin.clone().add(o[0] + 0.5, o[1], o[2] + 0.5);
             if (cand.getBlock().isPassable() && cand.clone().add(0, 1, 0).getBlock().isPassable()) {
